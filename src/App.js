41a88cc3ec085c1ef90e18731a21bbb5c10d00a8
WowliamGuo <64669373+WowliamGuo@users.noreply.github.com>
2023-01-14 14:00:24 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,94 +1,27 @@
-import Course from './components/Course'
 import { useState } from 'react'
+import Filter from './components/Filter'
+import Form from './components/Form'
+import ShowPersons from './components/ShowPersons'
 
 const App = () => {
 
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
-
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button = ', event.target)
-
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length
-    }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([]) 
+  const [newFilter, setNewFilter] = useState('')
 
   return (
     <>
-      {courses.map(course => <Course course={course} key = {course.id}/>)} 
+      <h2>Phonebook</h2>
+        <Filter newFilter = {newFilter} setNewFilter = {setNewFilter}/>
 
-      {notes.map(note => <p key = {note.id}> {note.content} </p>)}
+      <h2> Add New </h2>
+        <Form persons = {persons} setPersons = {setPersons} />
 
-      <form onSubmit = {addNote}>
-        <input value = {newNote} onChange = {handleNoteChange}/>
-        <button type = "submit"> save </button>
-
-      </form>
-
+      <h2>Numbers</h2>
+        <ShowPersons persons = {persons} newFilter = {newFilter} />
     </>
-    
   )
+
 }
+
 
 export default App
